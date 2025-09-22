cd89dd16b230796724459edd79d3b59e9eb5828c
r0b0t03000 <118480078+r0b0t03000@users.noreply.github.com>
2025-09-22 11:21:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
         print(f"Expiry date used in IV estimation (nearest to chosen option's expiration in monts): {expiry_IV}")
 
     print(f"Option price: {round(price, 4)} USD")
-    print(f"Volatility: {round(sigma*100, 4)}%")
+    print(f"Volatility: {round(sigma*100, 2)}%")
     for g, v in greeks.items():
         print(f"{g.capitalize()}: {round(v, 4)}")
 
